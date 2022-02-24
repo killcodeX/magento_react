936326a1d224d3d13d60c53c4095230ad1657d9d
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2022-02-24 17:59:43 +0530

    added home banner

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./home.css";
 
 export default function Home() {
   return (
-    <>
-      <div>Home</div>
-    </>
+    <section className='home'>
+      <div className='homebanner'
+      style={{ background: `URL(https://demo.deity.io/static/media/hero-img-01.a6e05efb.jpg)`}}></div>
+      home
+    </section>
   );
 }
+
+
+//${process.env.PUBLIC_URL + '/media/homebanner.jpg'}
