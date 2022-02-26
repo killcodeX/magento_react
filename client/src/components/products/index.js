ac712821146280539ae06ade60a1d4adc152235c
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2022-02-26 22:08:39 +0530

    product display done

--- client/src/components/products/index.js
+++ client/src/components/products/index.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { PRODUCTS_MAGENTO } from "../../graphql/productQueries";
+import "./products.css";
 
 export default function Products() {
   const { loading, error, data } = useQuery(PRODUCTS_MAGENTO);
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     console.log("from graphql api `data`-->>", data);
     console.log("from graphql api `error`-->>", JSON.stringify(error, null, 2));
     console.log("from graphql api `loading`-->>", loading);
-    if(data){
-        setProducts(data.products.items)
+    if (data) {
+      setProducts(data.products.items);
     }
   }, [data]);
 
@@ -19,19 +20,36 @@ export default function Products() {
 
   if (error) return <p>Error</p>;
   return (
-    <div className='container'>
-      <h3>Products</h3>
-      {console.log(products)}
-      {
-          products?.map(item => {
-              return(
-                  <div className='conatiners' key={item.id}>
-                      <p><strong>Name:</strong>{item.name}</p>
-                      {/* <img src={} alt='products' /> */}
+    <div className="container mt-3 pt-5">
+      <div className="row">
+        {products?.map((item) => {
+          return (
+            <div className="col-md-3 mb-2" key={item.id}>
+              <div className="product-container">
+                <div className="upper-section">
+                  <img
+                    src={
+                      item?.image
+                        ? item.image.url
+                        : "https://dev-d2c.upscalio.com/pub/media/catalog/product/cache/c2437caef9429815de44d4cbc4080d7e/1/_/1_d7dcf5f9-73df-44bf-a6cd-b8ea5938aecc_960x.jpeg"
+                    }
+                    alt="products"
+                  />
+                </div>
+                <div className="lower-section">
+                  <div className="product-categories">
+                    {item.categories.map((cat, index) => {
+                      return <p key={index}>{cat.name}</p>;
+                    })}
                   </div>
-              )
-          })
-      }
+                  <p className="product-name">{item.name}</p>
+                  <p className="product-price">$ {item.price_range.minimum_price.final_price.value}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- client/src/graphql/productQueries.js
+++ client/src/graphql/productQueries.js
@@ -3,18 +3,31 @@ import { gql } from "@apollo/client";
 // get all products
 export const PRODUCTS_MAGENTO = gql`
   query{
-    products(pageSize: 20, filter: { category_id: { eq: "2" } }) {
+    products(pageSize: 20, filter: {
+      category_id:{
+        eq:"2"
+      }
+    }){
       total_count
-      items {
+      items{
         id
         name
-        description {
-          html
-        }
         created_at
-        media_gallery {
+        image{
           url
         }
+        categories{
+          name
+        }
+        price_range{
+          minimum_price{
+            final_price{
+              value
+              currency
+            }
+          }
+        }
+        rating_summary
       }
     }
   }
