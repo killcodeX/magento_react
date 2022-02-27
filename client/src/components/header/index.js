02f876100910e495f3d807d57aeb779b114dc16f
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2022-02-27 16:58:17 +0530

    main header complete

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <nav className="navbar navbar-expand-lg  bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          Trase
+        </a>
+        <div className="px-5 collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Mens
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Womens
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Kids
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Ortho
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Best Offers
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
